55575aa4ce3df664d4169b20b12cc9a83a7194ad
MD Mozammel Hossain <mozammel169649@gmail.com>
2024-12-19 18:20:25 +0600

    react template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="my-3 font-bold text-2xl text-center">Ready Template</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
